686154a8d4800c5a52c8bad60f7173c5eb252044
mwang370 <mwang370@gatech.edu>
2016-02-26 10:24:19 -0500

    ..

--- submit.js
+++ submit.js
@@ -1,9 +1,9 @@
-function myFunction() {
+function submitSearch() {
     var x = document.getElementById("frm1");
     var text = "";
     var i;
     for (i = 0; i < x.length ;i++) {
         text += x.elements[i].value + "<br>";
     }
-    document.getElementById("demo").innerHTML = text;
+    document.getElementById("search").innerHTML = text;
 }
